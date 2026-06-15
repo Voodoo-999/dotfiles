df6e10419053c222ad969c19b4c0ea34bacd8799
Voodoo <60772919+Voodoo-999@users.noreply.github.com>
2026-06-15 17:44:40 +0300

    Added the context
    This keep the functions signature always sticking on top

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,22 +1,42 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-        -- looks cool but dont work for some reason...
-        -- ts-auto utilizes treesitter to understand the code stucture to automatically colse tsx tags
-        -- "window/nvim-ts-autotag"
-    },
-    build = ":TSUpdate",
-    config = function()
-        local configs = require("nvim-treesitter.configs")
-        configs.setup({
-            -- make sure those highlighting servers are installed
-            ensure_installed = {"go", "zig", "rust", "c", "python", "lua", "vim", "vimdoc", "java", "javascript", "typescript", "html", "css", "json", "tsx", "markdown", "markdown_inline"},
-            sync_install = false,
-            autotag = { enable = true},
-            highlight = { enable = true },
-            indent = { enable = true }
-        })
-    end
-
-
+	"nvim-treesitter/nvim-treesitter",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-context",
+	},
+	build = ":TSUpdate",
+	config = function()
+		require("treesitter-context").setup({
+			enable = true, -- Turn on the plugin
+			max_lines = 4, -- How many lines of nested context to show (e.g. Class > Function > If)
+			trim_scope = "outer", -- Discard outer lines if max_lines is exceeded
+			mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
+		})
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			-- make sure those highlighting servers are installed
+			ensure_installed = {
+				"go",
+				"zig",
+				"rust",
+				"c",
+				"python",
+				"lua",
+				"vim",
+				"vimdoc",
+				"java",
+				"javascript",
+				"typescript",
+				"html",
+				"css",
+				"json",
+				"tsx",
+				"markdown",
+				"markdown_inline",
+			},
+			sync_install = false,
+			autotag = { enable = true },
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
